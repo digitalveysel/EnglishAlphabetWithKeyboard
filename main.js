75042d78fc2c68e2a83a43c5aa3fead85f239631
Veysel Demirel <hello@veysel.co>
2022-04-04 02:26:16 +0300

    Clean junk codes.

--- main.js
+++ main.js
@@ -1,64 +1,45 @@
 import { alphabet } from "./constants/english-alphabet.js";
 
-const keys = document.querySelectorAll(".key");
-const keyArray = Array.from(keys);
+const keyArray = [...document.querySelectorAll(".key")];
 
-//ripple click effect for keys
-keyArray.forEach((key) => {
-  key.onclick = function (event) {
-    let ripple = document.createElement("span");
-    ripple.classList.add("ripple");
-    this.appendChild(ripple);
-
-    let x = event.clientX - event.target.offsetLeft;
-    let y = event.clientY - event.target.offsetTop;
-
-    ripple.style.left = `${x}px`;
-    ripple.style.top = `${y}px`;
-
-    setTimeout(() => {
-      ripple.remove();
-    }, 300);
-  };
-});
-
-//create data-key attributes and audio elements
-alphabet.forEach((item, index) => {
-  keyArray[index].setAttribute("data-key", alphabet[index].key);
-  let temporaryAudio = `
-  <audio data-key="${item.key}" src="./assets/sounds/${item.character}.mp3"></audio>
-    `;
-  document.getElementById("audios").innerHTML += temporaryAudio;
-});
-
-//play audio with data-key attribute
-function playAudioForKeyDown(event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add("key-playing");
-  setTimeout(() => {
-    key.classList.remove("key-playing");
-  }, 200);
-}
-
-function playAudioForClick() {
-  content = this.firstChild.innerHTML;
-  alphabet.forEach((item) => {
-    if (content == item.character) {
-      const audio = document.querySelector(`audio[data-key="${item.key}"]`);
-      if (!audio) return;
-      audio.currentTime = 0;
-      audio.play();
-    }
+/**
+ * @description Create audio elements with data-key attribute.
+ */
+function createAudioElements() {
+  return new Promise((resolve, reject) => {
+    alphabet.forEach((letter, index) => {
+      keyArray[index].setAttribute("data-key", alphabet[index].key);
+      const tempAudioElement = `
+    <audio data-key="${letter.key}" src="./assets/sounds/${letter.character}.mp3"></audio>
+      `;
+      const audiosElement = document.getElementById("audios");
+      audiosElement.innerHTML += tempAudioElement;
+    });
   });
 }
 
-keys.forEach((key) => {
-  key.addEventListener("click", playAudioForClick);
-});
+/**
+ * @description Play audio for keydown event.
+ * @param {Event} event
+ */
+function playAudioForKeyDown(event) {
+  if ((event.keyCode < 65) | (event.keyCode > 90)) return;
 
-//listen keydown for window object
+  const audioElement = document.querySelector(
+    `audio[data-key="${event.keyCode}"]`
+  );
+  const keyElement = document.querySelector(
+    `.key[data-key="${event.keyCode}"]`
+  );
+
+  audioElement.currentTime = 0;
+  audioElement.play();
+  keyElement.classList.add("key-playing");
+
+  setTimeout(() => {
+    keyElement.classList.remove("key-playing");
+  }, 300);
+}
+
+createAudioElements();
 window.addEventListener("keydown", playAudioForKeyDown);
